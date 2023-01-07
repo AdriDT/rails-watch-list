18528b52eb2e3834885559f060ed87b5f110209d
Adrien De Tchaguine <adriendetchaguine@gmail.com>
2023-01-07 15:13:42 +0100

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'movie/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :movies
+  resources :lists
+  resources :bookmarks, only: [:new, :create, :destroy]
 end
